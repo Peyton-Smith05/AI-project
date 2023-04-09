c5ac954586714f89584e1ee61de22cc865801568
fishorfished <angyujie970@gmail.com>
2023-04-10 01:07:50 +0800

    Update set

--- board.py
+++ board.py
@@ -111,8 +111,8 @@ class Board:
             self.player_pieces =  [[1,1], [2,1], [3,1], [4,1], [5,1], [6,1], [7,1], [8,1], [9,1], [2,3], [8,3], [1,4], [3,4], [5,4], [7,4], [9,4]]
             self.aipieces = [[1,10], [2,10], [3,10], [4,10], [5,10], [6,10], [7,10], [8,10], [9,10], [2,8], [8,8], [1,7], [3,7], [5,7], [7,7], [9,7]]
         
-        self.userthreats = []
-        self.aithreats = []
+        self.userthreats = set()
+        self.aithreats = set()
         self.ai_threat()
         self.user_threat()
     
@@ -157,7 +157,7 @@ class Board:
                     if (i[0] == file and i[1] == rank):
                         temp += Board.generate_pseudo_valid_moves_nocan(self.state, file, rank)
         for z in temp:
-            self.userthreats.append([z.target[0],z.target[1]])
+            self.userthreats.add((z.target[0],z.target[1]))
 
     def user_threat(self):
         temp = []
@@ -168,7 +168,7 @@ class Board:
                     if (i[0] == file and i[1] == rank):
                         temp += Board.generate_pseudo_valid_moves_nocan(self.state, file, rank)
         for z in temp:
-            self.aithreats.append([z.target[0],z.target[1]])
+            self.aithreats.add((z.target[0],z.target[1]))
 
     def updateBoardFromMove(self, m: Move):
         # Swap places in list    
@@ -627,13 +627,14 @@ class Board:
 
                 # 4. Create a move and add to list
                 if not disqualified and capture == False:
-                    for i in usermove:
-                        if (new_file == i[0] and new_rank == i[1]):
-                            move = Move((file, rank), (new_file, new_rank), capture, -1)
-                            break
-                        else: 
-                            move = Move((file, rank), (new_file, new_rank), capture, 5)
-                    moves.append(move)
+                    if (new_file, new_rank) in usermove:
+                        move = Move((file, rank), (new_file, new_rank), capture, -1)
+                        moves.append(move)
+                    else:
+                        move = Move((file, rank), (new_file, new_rank), capture, 5)
+                        moves.append(move)
+                    
+                    
                 elif not disqualified and capture == True:
                     captured_piece = board[(new_rank-1)*9 + (new_file-1)] 
                     captured_piece = PIECE_MAPPING[captured_piece.upper()]
